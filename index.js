c2dcfd817a20152a52e00e7e10401ba49d3bc707
yourts48-dotcom <yourts48@gmail.com>
2026-09-03 14:33:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ client.once('ready', () => {
 client.on('messageCreate', (message) => {
     if (message.author.bot) return;
 
-    if (message.content.toLowerCase() === 'ping') {
-        message.reply('Pong! 🏓');
+    if (message.content.toLowerCase() === 'سلام عليكم') {
+        message.reply('وعليكم السلام');
     }
 });
 
