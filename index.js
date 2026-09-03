0b7e127caeff8078ed4bcac3bdcb35dda0034247
yourts48-dotcom <yourts48@gmail.com>
2026-09-03 14:29:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
-// إنشاء العميل مع الصلاحيات (Intents) المطلوبة
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -9,21 +8,16 @@ const client = new Client({
     ]
 });
 
-// حادثة التشغيل: تظهر في الـ Console عند نجاح الاتصال
 client.once('ready', () => {
     console.log(`[+] Bot is online! Logged in as ${client.user.tag}`);
 });
 
-// حادثة استقبال الرسائل
 client.on('messageCreate', (message) => {
-    // تجاهل الرسائل القادمة من البوتات الأخرى
     if (message.author.bot) return;
 
-    // الرد على كلمة ping
     if (message.content.toLowerCase() === 'ping') {
         message.reply('Pong! 🏓');
     }
 });
 
-// تسجيل الدخول باستخدام التوكين الممرر عبر متغيرات البيئة
 client.login(process.env.DISCORD_TOKEN);
